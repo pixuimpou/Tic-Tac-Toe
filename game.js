179c521eecf2bd3d2234adf7249a200beb25cd9d
Rafael Pinheiro <rafael.c.pinheiro96@gmail.com>
2021-01-23 14:19:06 -0300

    added reset function

--- game.js
+++ game.js
@@ -64,3 +64,19 @@ const handlePlays = (square) => {
         }
     }
 }
+
+const handleReset = () => {
+    turn = "x";
+    
+    selecteds = {
+        1: { isSelected: false, by: "none" },
+        2: { isSelected: false, by: "none" },
+        3: { isSelected: false, by: "none" },
+        4: { isSelected: false, by: "none" },
+        5: { isSelected: false, by: "none" },
+        6: { isSelected: false, by: "none" },
+        7: { isSelected: false, by: "none" },
+        8: { isSelected: false, by: "none" },
+        9: { isSelected: false, by: "none" }
+    };
+}
--- layout.js
+++ layout.js
@@ -1,10 +1,11 @@
+container = document.getElementById("container");
+
 const annouceWinner = () => {
     if(isGameOver()) {
-        let container = document.getElementById("container");
 
-        container.innerHTML += `<div class="winner">
+        container.innerHTML += `<div class="winner" id="popUp">
                                     <h2>The ${turn.toUpperCase()} won</h2>
-                                    <div class="btn" onclick="resetGame()">Play Again</div>
+                                    <div class="btn" onclick="resetBoard()">Play Again</div>
                                 </div>`;
                             
     }
@@ -16,10 +17,28 @@ const makeMove = (square) => {
     let squareId = document.getElementById(square.id);
 
     if (squarePermited) {
-        squareId.innerHTML = `<div class="player">${turn}</div>`;
+        squareId.innerHTML = `<div class="player" id="p${squareId.id}">${turn}</div>`;
     }
 
     handlePlays(square);
     annouceWinner();
 }
 
+const resetBoard = () => {
+    handleReset();
+    container.innerHTML = `<div class="board">
+                <div class="square" id="1" onclick="makeMove(this)">
+                    <!-- <div class="player">X</div> -->
+                </div>
+                <div class="square" id="2" onclick="makeMove(this)">
+                    <!-- <div class="player">O</div> -->
+                </div>
+                <div class="square" id="3" onclick="makeMove(this)"></div>
+                <div class="square" id="4" onclick="makeMove(this)"></div>
+                <div class="square" id="5" onclick="makeMove(this)"></div>
+                <div class="square" id="6" onclick="makeMove(this)"></div>
+                <div class="square" id="7" onclick="makeMove(this)"></div>
+                <div class="square" id="8" onclick="makeMove(this)"></div>
+                <div class="square" id="9" onclick="makeMove(this)"></div>
+            </div>`;
+}
